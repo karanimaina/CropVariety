8901d92b02aab7dbfe57e1977c722ad03e6d3432
felixmaina <karani.maina2010@gmail.com>
2022-11-15 19:47:54 +0300

    edit Crop

--- src/main/java/com/example/cropvariety/controller/CropVarietyController.java
+++ src/main/java/com/example/cropvariety/controller/CropVarietyController.java
@@ -1,9 +1,15 @@
 package com.example.cropvariety.controller;
 
+import com.example.cropvariety.commons.UniversalResponse;
+import com.example.cropvariety.dto.CropVarietyDto;
+import com.example.cropvariety.model.CropVariety;
 import com.example.cropvariety.repo.CropVarietyRepository;
 import com.example.cropvariety.service.CropVarietyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class CropVarietyController {
-    private final CropVarietyService cropVarietyService
+    private final CropVarietyService cropVarietyService;
+    @PostMapping("/add/crop")
+     ResponseEntity<UniversalResponse>addCrop(@RequestBody CropVarietyDto cropVarietyDto){
+        CropVariety cropVariety = cropVarietyService.addCropVariety(cropVarietyDto);
+        return ResponseEntity.ok().body(UniversalResponse.builder().status(200).message("crop added").data(cropVariety).build());
+     }
+     ResponseEntity<UniversalResponse> editCrop(long id, CropVarietyDto cropVarietyDto){
+        CropVariety cropVariety  = cropVarietyService.editCropVariety(id,cropVarietyDto);
+         return ResponseEntity.ok().body(UniversalResponse.builder().status(200).message("crop added").data(cropVariety).build());
+     }
 }
